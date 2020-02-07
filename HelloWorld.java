0b85b85e79b03ad58a51b334fcd590bb0dfe85c8
XiongKezhi <398682946@qq.com>
2020-02-07 19:38:13 +0800

    hello changes!

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
         System.out.println("Hello, Jenkins");
         System.out.println("Hello, August");
         System.out.println("Hello, Build");
+        System.out.println("Hello, Changes");
     }
 }
